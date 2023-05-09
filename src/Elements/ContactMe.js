e23367a48a19cbef239ea5056664038e3c1b2bee
windylung <jisoolee031@gmail.com>
2023-05-09 18:25:37 +0900

    add Blog Adress

--- src/Elements/ContactMe.js
+++ src/Elements/ContactMe.js
@@ -1,6 +1,8 @@
 import ET from "../Image/ET.png";
 export function ContactMe() {
   const text = "Contact Me";
+  const url = "https://blog.naver.com/windylung";
+
   return (
     <div
       style={{
@@ -27,11 +29,17 @@ export function ContactMe() {
 
         <div style={{ fontSize: 20 }}>jisoolee1126@dankook.ac.kr</div>
         <div style={{ fontSize: 20 }}>@2_tothenth_power</div>
+        <div style={{ fontSize: 20 }}>
+          https://blog.naver.com/windylung
+        </div>
+        <div style={{ fontSize: 20 }}>
+        https://github.com/windylung
+        </div>
       </div>
       <div
         style={{
           width: "30%",
-          height: '100%',
+          height: "100%",
         }}
       >
         <img src={ET} style={{ height: "100%" }}></img>
